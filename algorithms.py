7cf1a8e959782374e25886fa3695119b36ad9233
Cyrille Pittet <cyrille.pittet@gmail.com>
2021-12-09 16:13:08 +0100

    Correct Hamiltonian and addded fast version of it

--- algorithms.py
+++ algorithms.py
@@ -3,6 +3,7 @@ import networkx as nx
 import itertools as it
 from helper import *
 from tqdm import tqdm
+from multiprocessing import Pool
 
 
 def overlap(x, x_star):
@@ -12,23 +13,41 @@ def overlap(x, x_star):
 def compute_acceptance_proba(x, y, n, adj, a, b):
     num = 0
     denom = 0
+    h = get_h(adj, a, b, n)
     for i in range(0, n - 1):
         for j in range(i + 1, n):
-            hij = get_h_ij(adj, i, j, a, b, n)
-            num += hij * x[i] * x[j]
-            denom += hij * y[i] * y[j]
+            denom += h[i, j] * x[i] * x[j]
+            num += h[i, j] * y[i] * y[j]
     return min(1.0, np.exp(num - denom))
+
+
+def compute_acceptance_proba_fast(x, v, n, adj, a, b):
+    # v is the index of the changed component
+    mask = np.ones(x.shape, dtype=bool)
+    mask[v] = False
+
+    h_row_masked = get_h_row(adj, a, b, n, v).reshape(-1)
+    return - 2 * x[v] * np.sum(x[mask] * h_row_masked[mask])
 
 
 def get_h_ij(adj, i, j, a, b, n):
     return 0.5 * (adj[i, j] * np.log(a / b) + (1 - adj[i, j]) * np.log((1 - a / n) / (1 - b / n)))
 
 
+def get_h(adj, a, b, n):
+    return 0.5 * (adj * np.log(a / b) + (1 - adj) * np.log((n - a) / (n - b)))
+
+
+def get_h_row(adj, a, b, n, v):
+    return 0.5 * (adj[v, :] * np.log(a / b) + (1 - adj[v, :]) * np.log((n - a) / (n - b)))
+
+
 def metropolis_step(adj, a, b, cur_x, nb):
     comp = np.random.choice(nb, 1)
     proposed_move = cur_x.copy()
     proposed_move[comp] = -proposed_move[comp]
-    acc = compute_acceptance_proba(cur_x, proposed_move, nb, adj, a, b)
+    #acc = compute_acceptance_proba(cur_x, proposed_move, nb, adj, a, b)
+    acc = compute_acceptance_proba_fast(cur_x, comp, nb, adj, a, b)
 
     if np.random.uniform(0.0, 1.0) <= acc:
         return proposed_move
@@ -48,18 +67,19 @@ def metropolis(adj, a, b, nb, nb_iter, x_star, *args):
 
 
 def run_experiment(nb, a, b, x_star, algorithm, n0=0, nb_iter=200, nb_exp=100):
-    sum_x = np.zeros(nb)
+    #sum_x = np.zeros(nb)
     overlap = np.zeros((nb_exp, nb_iter))
     for j in tqdm(range(nb_exp)):
-        adj = generate_graph(x_star, a, b)
+        if x_star is not None:
+            adj = generate_graph(x_star, a, b)
         new_x, overlap_list = algorithm(adj, a, b, nb, nb_iter, x_star, n0)
-        sum_x += new_x
+        #sum_x += new_x
         overlap[j, :] = np.array(overlap_list)
 
-    x_hat = sum_x / nb_exp
-    x_hat[x_hat > 0.0] = 1
-    x_hat[x_hat <= 0.0] = -1
-    return x_hat, overlap.mean(axis=0)
+    #x_hat = sum_x / nb_exp
+    #x_hat[x_hat > 0.0] = 1
+    #x_hat[x_hat <= 0.0] = -1
+    return overlap.mean(axis=0)
 
 
 def houdayer_step(cur_x1, cur_x2):
