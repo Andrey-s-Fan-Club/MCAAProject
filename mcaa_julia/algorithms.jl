76030c8156c30563c915b7a6a6d9fffe2c52f424
Cyrille Pittet <cyrille.pittet@gmail.com>
2021-12-09 21:37:22 +0100

    Fixed python code and added Julia metropolis

---
+++ mcaa_julia/algorithms.jl
@@ -0,0 +1,69 @@
+using StatsBase
+
+include("helper.jl")
+
+function overlap(x::Vector{Int8}, x_start::Vector{Int8})
+    return abs(mean(x .* x_star))
+end
+
+
+function get_h_row(adj::BitMatrix, a::Integer, b::Integer, n::Integer, v::Integer)
+    # Broadcast operations with dot
+    return 0.5 .* (log(a / b) .* adj[v, :] .+ log((n - a) / (n - b)) .* (1 .- adj[v, :]))
+end
+
+
+function compute_acceptance_proba(x::Vector{Int8}, v::Integer, n::Integer, adj::BitMatrix, a::Integer, b::Integer)
+    mask = trues(length(x))
+    mask[v] = false
+    
+    h_row_masked = get_h_row(adj, a, b, n, v)
+    return - x[v] * sum(x[mask] .* h_row_masked[mask])
+end
+
+
+function metropolis_step(adj::BitMatrix, a::Integer, b::Integer, cur_x::Vector{Int8}, nb::Integer)
+    comp = sample(1:nb)
+    proposed_move = copy(cur_x)
+    proposed_move[comp] = -proposed_move[comp]
+    
+    acc = compute_acceptance_proba(cur_x, comp, nb, adj, a, b)
+    
+    if rand(Uniform(0, 1)) <= acc
+        return proposed_move
+    else
+        return cur_x
+    end
+end
+
+
+function metropolis(adj::BitMatrix, a::Integer, b::Integer, nb::Integer, nb_iter::Integer, x_star::Vector{Int8}, arg=nothing)
+    cur_x = generate_x(nb)
+    overlap_vector = Vector{Float64}(undef, nb_iter)
+    
+    for i = 1:nb_iter
+        cur_x = metropolis_step(adj, a, b, cur_x, nb)
+        if x_star != nothing
+            overlap_vector[i] = overlap(cur_x, x_star)
+        end
+    end
+    
+    return cur_x, overlap_vector
+end
+
+function run_experiment(nb::Integer, a::Integer, b::Integer, x_star::Vector{Int8}, algorithm::Function, nb_iter::Integer=1000, nb_exp::Integer=100, n0::Integer=0)
+    overlaps = zeros(nb_exp, nb_iter)
+
+    Threads.@threads for j = 1:nb_exp
+        if x_star != nothing
+            adj = generate_graph(x_star, a, b)
+        end
+        
+        new_x, overlap_list = algorithm(adj, a, b, nb, nb_iter, x_star, n0)
+        
+        overlaps[j, :] = overlap_list
+    end
+    
+    return mean(overlaps, dims=1)
+end
+    
---
+++ mcaa_julia/helper.jl
@@ -0,0 +1,43 @@
+using StatsBase
+using Distributions
+using Plots
+
+function generate_x(size::Integer)
+    return sample(Vector{Int8}([-1, 1]), size)
+end
+
+function generate_graph(x::Vector{Int8}, a::Integer, b::Integer)
+    n = length(x)
+    adjacency_mat = falses(n, n)
+    
+    b_n = (b / n)
+    a_n = (a / n
+        
+    # Indexing start at 1 and n is included
+    Threads.@threads for i = 1:n
+        for j = (i+1):n
+            prod = x[i] * x[j]
+            
+            if prod == -1
+                value = rand(Uniform(0, 1)) < b_n
+            else
+                value = rand(Uniform(0, 1)) < a_n
+            end
+             
+            adjacency_mat[i, j] = value
+            adjacency_mat[j, i] = value
+            
+        end
+    end
+        
+    return adjacency_mat
+end
+            
+function visualize_overlap(overlap_array::Vector{Float64}, nb_exp::Integer)
+    plot(overlap_array,
+        title="Average overlap over $(nb_exp) experiments",
+        xlabel="Iterations of the MC",
+        ylabel="Avg overlap")
+end
+
+                
